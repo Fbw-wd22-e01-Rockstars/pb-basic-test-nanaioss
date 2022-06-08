6cf1641f33041db1a758579dd58497dd7bdf9013
nana.ios <weinuu@gmail.com>
2022-06-08 09:45:36 +0200

    first answer

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // Add answers here 
 
+//!#### 1. Los or New?
 
-
+function nameOfCity(str) {
+    if (str.length >= 3 && ((str.substring(0, 3) == 'Los')
+                            || (str.substring(0, 3) == 'New')))
+     
+        {
+            return str;
+      }
+  
+    return 'The city name does not begin with Los or New';
+  }
+  
+  console.log(nameOfCity("New York"));
+  console.log(nameOfCity("Los Angeles"));
+  console.log(nameOfCity("London"));
 
 
 // DO NOT EDIT below this line - This will result in an automatic fail
